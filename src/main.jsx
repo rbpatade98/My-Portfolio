0a2634a5c5caf9e9e25763d91710d7ac737acea0
Ramchandra Patade <rbpatade98@gmail.com>
2026-06-18 17:04:25 +0530

    fixed version1

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Suppress known third-party library deprecation warnings
+const originalWarn = console.warn;
+console.warn = (...args) => {
+  if (args[0] && typeof args[0] === 'string' && args[0].includes('THREE.Clock')) {
+    return;
+  }
+  originalWarn(...args);
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
